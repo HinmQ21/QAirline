5c79863c85e30b9008017fb8b25b52ec20556d70
Dao Le Bao Minh <djnner@proton.me>
2025-05-23 19:22:10 +0700

    hihi

--- frontend/src/components/admin/DashboardNavigation.jsx
+++ frontend/src/components/admin/DashboardNavigation.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { Fragment } from 'react';
 import { IoHomeOutline, IoHome } from "react-icons/io5";
 import { FaRegPenToSquare, FaPenToSquare } from "react-icons/fa6";
 
@@ -9,45 +9,49 @@ const navItems = [
 
 export const DashboardNavigationIcon = ({ icon: Icon, iconOnHover: IconOnHover, isSelected, onClick }) => (
   <div onClick={onClick} className={`
-      rounded-4xl shadow-xl transition-all duration-300 relative group
-      flex flex-row items-center justify-center
-      ${isSelected === true ? (
+    rounded-4xl shadow-xl cursor-pointer
+    relative group
+    flex items-center justify-center
+    transition-all duration-300
+
+    ${isSelected === true ? (
       "w-14 h-14 bg-indigo-500 shadow-indigo-500/60"
     ) : (
-      "w-12 h-12 bg-gray-900 shadow-gray-900/60 cursor-pointer"
-    )}
-      hover:w-14 hover:h-14 hover:bg-teal-600 hover:shadow-teal-600/60
+      "w-12 h-12 bg-gray-900 shadow-gray-900/60 \
+      hover:w-14 hover:h-14 hover:bg-teal-600 hover:shadow-teal-600/60"
+    )
+    }
   `}>{
       (isSelected === true) ? (
-        <Icon size="24" className="absolute opacity-100 text-white" />
+        <IconOnHover size="24" className="absolute opacity-100 text-white" />
       ) : (
         <>
           <Icon size="20" className="
             absolute opacity-100 text-white
             group-hover:opacity-0 transition-opacity duration-300
-        "/>
+          "/>
           <IconOnHover size="24" className="
             absolute opacity-0 text-white
             group-hover:opacity-100 transition-opacity duration-300
-        "/>
+          "/>
         </>
       )
     }</div>
 )
 
 export const DashboardNavigation = ({ selectedTab, onTabSelect }) => {
-  return <div className="flex flex-col min-h-screen justify-center w-30 items-center">
+  return <div className="flex flex-col h-screen justify-center w-30 items-center">
     {
       navItems.map((item, idx) => (
-        <React.Fragment key={idx}>
+        <Fragment key={idx}>
           <DashboardNavigationIcon
             icon={item.icon}
             iconOnHover={item.iconOnHover}
             isSelected={selectedTab === idx}
             onClick={() => onTabSelect(idx)}
           />
-          {(idx === 0) && <div className=" w-[2px] h-5 bg-gray-600"></div>}
-        </React.Fragment>
+          {(idx === 0) && <div className="w-[2px] h-5 bg-gray-600" />}
+        </Fragment>
       ))
     }
   </div>;
--- frontend/src/pages/admin/DashboardPage.jsx
+++ frontend/src/pages/admin/DashboardPage.jsx
@@ -1,17 +1,42 @@
 import { DashboardNavigation } from "@/components/admin/DashboardNavigation";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export const AdminDashboardPage = () => {
   const [selectedTab, setSelectedTab] = useState(0);
+  const [navY, setNavY] = useState(0);
 
   const onTabSelect = (idx) => {
-    console.log(idx);
     setSelectedTab(idx);
-  }
-  
+  };
+
+  useEffect(() => {
+    let animationFrame;
+
+    const updatePosition = () => {
+      setNavY(window.scrollY);
+
+      setTimeout(() => {
+        animationFrame = requestAnimationFrame(updatePosition);
+      }, 100);
+    };
+
+    animationFrame = requestAnimationFrame(updatePosition);
+    return () => cancelAnimationFrame(animationFrame);
+  }, []);
+
   return (
-    <div className="flex flex-row min-h-screen min-w-screen bg-gray-300">
-      <DashboardNavigation selectedTab={selectedTab} onTabSelect={onTabSelect}/>
+    <div className="relative min-h-screen bg-gray-300">
+      <div
+        className="absolute left-0 transition-transform duration-400 ease-out"
+        style={{ transform: `translateY(${navY}px)` }}
+      >
+        <DashboardNavigation selectedTab={selectedTab} onTabSelect={onTabSelect} />
+      </div>
+
+      <div className="ml-30">
+        <div className="h-screen">page 1</div>
+        <div className="h-screen">page 2</div>
+      </div>
     </div>
   );
-}
+};
