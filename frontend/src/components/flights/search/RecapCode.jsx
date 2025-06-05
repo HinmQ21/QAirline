5cce9b695f769fe5ce2a3bf7e1d894ead51ff35e
ThanhHigh <ducthanhtre2004@gmail.com>
2025-06-05 22:50:40 +0700

    make book header little responsive

--- frontend/src/components/flights/search/RecapCode.jsx
+++ frontend/src/components/flights/search/RecapCode.jsx
@@ -2,38 +2,24 @@ import { LuPlaneTakeoff } from "react-icons/lu";
 
 export const RecapCode = ({from, to, roundtrip}) => {
     return(
-        <>
-            <div className="w-2/10 h-fit
-                          text-white text-xl inter-extrabold">{from}</div>
-            <div className="w-6/10 h-full flex flex-col">
-                {roundtrip ? (
-                    <>
-                        <div className="w-full h-1/2 flex justify-center items-center ">
-                            <div className="text-white font-bold text-lg ">. . . . . . .</div>
-                            <div className="ml-1 w-1/10 h-1/2">
-                                <LuPlaneTakeoff className="text-white" />
-                            </div>
-                        </div>
-                        <div className="w-full h-1/2 flex justify-center items-center">
-                            <div className="mr-1 w-1/10 h-1/2">
-                                <LuPlaneTakeoff className="text-white" />
-                            </div>
-                            <div className="text-white font-bold text-lg ">. . . . . . .</div>
-                        </div>
-                    </>
-                ) : (
-                    <>
-                        <div className="w-full h-full flex justify-center items-center ">
-                            <div className="text-white font-bold text-lg ">. . . . . . .</div>
-                            <div className="ml-1 w-1/10 h-1/2">
-                                <LuPlaneTakeoff className="text-white" />
-                            </div>
-                        </div>
-                    </>
-                )}               
-            </div>
-            <div className="w-2/10 h-fit
-                          text-white text-xl inter-extrabold">{to}</div>
-        </>
+      <>
+        <div className="w-2/10 h-fit text-white text-xl inter-extrabold text-left">{from}</div>
+        <div className="w-6/10 h-full flex flex-col">
+          {roundtrip ? (
+            <>
+              <div className="w-full h-1/2 flex justify-center items-center ">
+              </div>
+              <div className="w-full h-1/2 flex justify-center items-center">
+              </div>
+            </>
+          ) : (
+            <>
+              <div className="w-full h-full flex justify-center items-center ">
+              </div>
+            </>
+          )}               
+        </div>
+        <div className="w-2/10 h-fit text-white text-xl inter-extrabold text-right">{to}</div>
+      </>
     );
 }
--- frontend/src/components/flights/search/header/BookedHeader.jsx
+++ frontend/src/components/flights/search/header/BookedHeader.jsx
@@ -28,25 +28,25 @@ export const BookedHeader = ({ }) => {
   return (
     <>
       {/* RecapHeader */}
-      <div className="h-20 w-full px-10 items-center justify-center flex">
-        <div className="w-9/10 h-full flex flex-row justify-between ">
+      
+        <div className="h-fit w-full xl:w-9/10 flex flex-row justify-between items-center">
           <div className="w-7/10 h-full py-2 flex">
             {/* Flight Recap */}
             <FlightRecap from={departureAirport} to={arrivalAirport} roundtrip={true} 
             start={departure_time} end={arrival_time} passanger={passenger}/>
             
           </div>
-          <div className="w-1/5 h-full flex justify-center items-center">
-            <button className="w-3/5 h-full flex flex-col justify-center items-center     
+          
+            <button className="w-[100px] lg:w-[150px] h-20 flex flex-col justify-center items-center     
                               bg-red-600 hover:bg-red-700 cursor-pointer
                               transition duration-300 ease-in-out
                               ">
               <LuPlaneTakeoff className="w-6 h-6 text-white"/>
               <p className="text-white">Dat cho</p>
             </button>
-          </div>
+          
         </div>
-      </div>    
+         
     </>
   );
 }
